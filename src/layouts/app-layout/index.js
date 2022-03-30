3a666ac4f4098995ab1c0ab7aa773b1c2e40d958
AhmedZohny208 <asfmz754@gmail.com>
2022-03-30 15:58:13 +0200

    Added Navbar html elements

--- src/layouts/app-layout/index.js
+++ src/layouts/app-layout/index.js
@@ -3,12 +3,18 @@ import {
   Layout
 } from "antd";
 import AppViews from '../../views/app-views';
+import Navbar from '../../components/layout-components/Navbar';
+import Sidebar from '../../components/layout-components/Sidebar';
+
+
 
 const { Content } = Layout;
 
 export default function AppLayout() {
   return (
     <Layout>
+      <Navbar />
+      <Sidebar />
       <Content>
         <AppViews />
       </Content>
