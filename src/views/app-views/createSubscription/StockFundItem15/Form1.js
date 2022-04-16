b19d8e87c830328f2f73eeefb350badf27a3007a
AhmedZohny208 <asfmz754@gmail.com>
2022-04-16 11:23:59 +0200

    Finished item 15

---
+++ src/views/app-views/createSubscription/StockFundItem15/Form1.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Select, Input } from 'antd'
+import item from '../../../../configs/stockFundItems/Item15'
+
+const { Option } = Select
+
+export default function Form1() {
+  return (
+    <div>
+      <p className='p-0 fw-bold mb-3'>{item.p3}</p>
+
+      <div className="row">
+
+        <div className="col-5">
+          <div className='mb-3'>
+            <p className='p-0 mb-1 fw-600'>{item.l1}</p>
+            <Select className='w-100 m-0' placeholder={item.ph1} >
+              <Option value='jack'>Jack</Option>
+            </Select>
+          </div>
+        </div>
+
+        <div className="col-7">
+          <div className="row">
+            <div className="col-6">
+              <div className='mb-3'>
+                <p className='p-0 mb-1 fw-600'>{item.l2}</p>
+                <Input className='grey-input m-0' value={item.v2} />
+              </div>
+            </div>
+            <div className="col-6">
+              <div className='mb-3'>
+                <p className='p-0 mb-1 fw-600'>{item.l3}</p>
+                <Input className='grey-input m-0' value={item.v3} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-5">
+          <div className='mb-3'>
+            <p className='p-0 mb-1 fw-600'>{item.l4}</p>
+            <Input className='grey-input m-0' value={item.v4} />
+          </div>
+        </div>
+
+        <div className="col-7">
+          <div className="row">
+            <div className="col-6">
+              <div className='mb-3'>
+                <p className='p-0 mb-1 fw-600'>{item.l5}</p>
+                <Input className='grey-input m-0' value={item.v5} />
+              </div>
+            </div>
+            <div className="col-6">
+              <div className='mb-3'>
+                <p className='p-0 mb-1 fw-600'>{item.l6}</p>
+                <Input className='grey-input m-0' value={item.v6} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/views/app-views/createSubscription/StockFundItem15/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Header from '../../../../components/shared-components/Headers/ItemPage'
+import NavigateItems from '../../../../components/shared-components/Navigate/NavigateItems'
+import item from '../../../../configs/stockFundItems/Item15'
+import Form1 from './Form1'
+import Form2 from './Form2'
+
+export default function index() {
+  return (
+    <div className='stock-fund-item'>
+      <Header
+        title={'15. مراقبا حسابات الصندوق'}
+        progress={10}
+        info={'البند الخامس عشر'}
+      />
+
+      <div className="content">
+        <div className="p-in-first">
+          <div className='mb-36'>
+            <p className='p-0 mb-2'>{item.p1}</p>
+            <p className='p-0'>{item.p2}</p>
+          </div>
+
+          <Form1 />
+        </div>
+
+        <div className="p-in">
+          <Form2 />
+        </div>
+
+        <div className="p-in">
+          <p>
+            {item.p5} <br />
+            {item.p6}
+          </p>
+
+          {item.orderedList.map(ele => (
+            <p>{ele}</p>
+          ))}
+
+        </div>
+      </div>
+
+      <NavigateItems item={15} />
+    </div>
+  )
+}
