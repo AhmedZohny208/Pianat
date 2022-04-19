72ef5745da8ff8d8d394844063571efe3909981a
AhmedZohny208 <asfmz754@gmail.com>
2022-04-19 03:47:14 +0200

    Finished items 32 & 33 & 34 UI

--- src/views/app-views/createSubscription/StockFundItem33/index.js
+++ src/views/app-views/createSubscription/StockFundItem33/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Input, Select } from 'antd'
+import SignatureUpload from '../../../../components/shared-components/Buttons/SignatureUpload'
 import Header from '../../../../components/shared-components/Headers/ItemPage'
 import NavigateItems from '../../../../components/shared-components/Navigate/NavigateItems'
+import item from '../../../../configs/stockFundItems/Item33'
+
+const { Option } = Select
 
 export default function StockFundItem33() {
   return (
@@ -10,6 +15,42 @@ export default function StockFundItem33() {
         progress={10}
         info={'البند الثالث والثلاثون'}
       />
+
+      <div className="content">
+        <div className="p-in-first">
+          <div className="content-w-input mb-12">
+            {item.p1}
+            <Input className='grey-input w-35 mx-2' value={item.p2} />
+            {item.p3}{' '}
+            {item.p4}
+          </div>
+
+          <p className='p-0 mb-3'>{item.p5}</p>
+
+          <div className='row mb-2'>
+            <div className="col-4">
+              <label className='fw-600 mb-2'>{item.l1}</label>
+              <Select placeholder={item.v1} className='w-100 m-0' >
+                <Option value='jack'>Jack</Option>
+              </Select>
+            </div>
+            <div className="col-4">
+              <label className='fw-600 mb-2'>{item.l2}</label>
+              <Select placeholder={item.v2} className='w-100 m-0' >
+                <Option value='jack'>Jack</Option>
+              </Select>
+            </div>
+            <div className="col-4">
+              <label className='fw-600 mb-2'>{item.l3}</label>
+              <Input className='grey-input' placeholder={item.v3} />
+            </div>
+          </div>
+          <div className='mb-5'>
+            <SignatureUpload />
+          </div>
+        </div>
+      </div>
+
       <NavigateItems item={33} />
     </div>
   )
--- src/views/app-views/createSubscription/StockFundItem34/index.js
+++ src/views/app-views/createSubscription/StockFundItem34/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { Select } from 'antd'
+import SignatureUpload from '../../../../components/shared-components/Buttons/SignatureUpload'
 import Header from '../../../../components/shared-components/Headers/ItemPage'
 import NavigateItems from '../../../../components/shared-components/Navigate/NavigateItems'
+import item from '../../../../configs/stockFundItems/Item34'
+
+const { Option } = Select
 
 export default function StockFundItem34() {
   return (
@@ -10,6 +15,43 @@ export default function StockFundItem34() {
         progress={10}
         info={'البند الرابع والثلاثون'}
       />
+
+      <div className="content">
+        <div className="p-in-first">
+          <p className='py-4 mb-0'>{item.p1}</p>
+
+          <div className="row mb-5">
+            <div className="col-5 mb-4">
+              <label className='fw-600 mb-2'>{item.p2}</label>
+              <Select placeholder={item.p3} className='w-100 m-0' >
+                <Option value='jack'>Jack</Option>
+              </Select>
+            </div>
+            <div className="col-6 mb-4">
+              <SignatureUpload />
+            </div>
+            <div className="col-5 mb-4">
+              <label className='fw-600 mb-2'>{item.p2}</label>
+              <Select placeholder={item.p3} className='w-100 m-0' >
+                <Option value='jack'>Jack</Option>
+              </Select>
+            </div>
+            <div className="col-6 mb-4">
+              <SignatureUpload />
+            </div>
+            <div className="col-5 mb-4">
+              <label className='fw-600 mb-2'>{item.p2}</label>
+              <Select placeholder={item.p3} className='w-100 m-0' >
+                <Option value='jack'>Jack</Option>
+              </Select>
+            </div>
+            <div className="col-6 mb-4">
+              <SignatureUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+
       <NavigateItems item={34} />
     </div>
   )
