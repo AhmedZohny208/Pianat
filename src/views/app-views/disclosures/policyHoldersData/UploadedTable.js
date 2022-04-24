5a36129975d09f1369fa61b594ecf9afe3923878
AhmedZohny208 <asfmz754@gmail.com>
2022-04-25 01:22:39 +0200

    Added create uploaded table data

--- src/views/app-views/disclosures/policyHoldersData/UploadedTable.js
+++ src/views/app-views/disclosures/policyHoldersData/UploadedTable.js
@@ -2,8 +2,69 @@ import React from 'react'
 import AddDocHolderBtn from '../../../../components/shared-components/Buttons/AddDocHolder'
 import TableSearch from '../../../../components/shared-components/Form/TableSearch'
 import DownloadDocGoldBtn from '../../../../components/shared-components/Buttons/DownloadDocGold'
+import { ReactComponent as Eye } from '../../../../components/shared-components/svgs/eyeTable.svg'
+import { ReactComponent as Edit } from '../../../../components/shared-components/svgs/editTable.svg'
+import { ReactComponent as EditRed } from '../../../../components/shared-components/svgs/editTableRed.svg'
+import { ReactComponent as Delete } from '../../../../components/shared-components/svgs/deleteTable.svg'
+import data from './data/UploadedTableData'
+import { Table } from 'antd'
 
 export default function UploadedTable() {
+
+  const columns = [
+    {
+      title: 'الاسم',
+      dataIndex: 'name',
+      key: 'الاسم'
+    },
+    {
+      title: 'نوع المستثمر',
+      dataIndex: 'investorType',
+      key: 'نوع المستثمر'
+    },
+    {
+      title: 'الجنسية',
+      dataIndex: 'nationality',
+      key: 'الجنسية'
+    },
+    {
+      title: 'رقم البطاقة',
+      dataIndex: 'nationalId',
+      key: 'رقم البطاقة'
+    },
+    {
+      title: 'رقم السجل التجاري',
+      dataIndex: 'registerationNum',
+      key: 'رقم السجل التجاري'
+    },
+    {
+      title: 'عدد الوثائق المشتراه او المستردة',
+      dataIndex: 'docNum',
+      key: 'عدد الوثائق المشتراه او المستردة'
+    },
+    {
+      title: 'Error',
+      dataIndex: 'error',
+      hidden: true,
+      key: 'error'
+    },
+    {
+      title: '% ملكية الوثائق بعد الشراء ',
+      dataIndex: 'docOwnership',
+      key: '% ملكية الوثائق بعد الشراء ',
+      render: (text, record) => (
+        <div className='d-flex justify-content-between align-items-center'>
+          <div>{text}</div>
+          <div className='action-icons'>
+            <Eye />
+            {record.error === true ? <EditRed className='mx-2 red' /> : <Edit className='mx-2' />}
+            <Delete />
+          </div>
+        </div>
+      )
+    },
+  ].filter(item => !item.hidden);
+
   return (
     <div className='contributers-table'>
       <div className='gold'>
@@ -20,7 +81,9 @@ export default function UploadedTable() {
             <DownloadDocGoldBtn />
           </div>
         </div>
-        <div className='table'></div>
+        <div className='table large-table mt-3'>
+          <Table columns={columns} dataSource={data} pagination={false} />
+        </div>
       </div>
     </div>
   )
