a11d2722b2a29c5cc1ab248ec3b4ebe69db59463
AhmedZohny208 <asfmz754@gmail.com>
2022-04-26 14:39:31 +0200

    Refractored code in policy holders pages

--- src/views/app-views/disclosures/policyHoldersData/components/MainCreateCard.js
+++ src/views/app-views/disclosures/policyHoldersData/components/MainCreateCard.js
@@ -6,34 +6,20 @@ import UploadShareholderFormBtn from '../../../../../components/shared-component
 import { ReactComponent as Exclamation } from '../../../../../components/shared-components/svgs/exclamationBlue.svg'
 import AddName from '../../../../../components/shared-components/Buttons/AddName'
 import UploadedTable from './UploadedTable'
-import DeleteDisclosure from '../../../../../components/shared-components/Buttons/DeleteDisclosure'
-import FutureSaveDisclosure from '../../../../../components/shared-components/Buttons/FutureSaveDisclosure'
-import SendDisclosure from '../../../../../components/shared-components/Buttons/SendDisclosure'
 import NewDocHolderModal from '../../../../../components/shared-components/Modals/NewDocHolderModal'
-import DeletePopup from '../../../../../components/shared-components/Popups/DeletePopup'
-import SuccessSmPopup from '../../../../../components/shared-components/Popups/SuccessSmPopup'
+import MainCreateCardActions from './MainCreateCardActions'
 
 export default function MainCreateCard() {
   const [createCard, showCreateCard] = useState(true)
   const [uploadedTable, showUploadedTable] = useState(false)
-
   const [newDocVisible, isNewDocVisible] = useState(false)
-  const [deleteVisible, isDeleteVisible] = useState(false)
-  const [successVisible, isSuccessVisible] = useState(false)
 
   const onUpload = () => {
     showCreateCard(!createCard)
     showUploadedTable(!uploadedTable)
   }
-
   const handleCancelCreateNewDoc = () => {
     isNewDocVisible(false)
-  }
-  const handleCancelDelete = () => {
-    isDeleteVisible(false)
-  }
-  const handleCancelSuccess = () => {
-    isSuccessVisible(false)
   }
 
   return (
@@ -73,27 +59,9 @@ export default function MainCreateCard() {
           <UploadedTable />
         )}
 
-        <div className='d-flex justify-content-between mt-80'>
-          <div>
-            <DeleteDisclosure 
-              content={'حذف الافصاح'} 
-              onclick={() => isDeleteVisible(true)}
-            />
-            <FutureSaveDisclosure content={'حفظ لوقت لاحق'} classn={'ms-4'} />
-          </div>
-          <div>
-            <SendDisclosure 
-              content={'ارسال الافصاح'}
-              onclick={() => isSuccessVisible(true)}
-            />
-          </div>
-        </div>
-        
+        <MainCreateCardActions />
       </div>
-
       <NewDocHolderModal visible={newDocVisible} onCancel={handleCancelCreateNewDoc} />
-      <DeletePopup visible={deleteVisible} onCancel={handleCancelDelete} />
-      <SuccessSmPopup visible={successVisible} onCancel={handleCancelSuccess} />
     </div>
   )
 }
